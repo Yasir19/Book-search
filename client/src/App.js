68a03d6ba1a27177f569c6334a65840a157ac6a4
yasir19 <yasir.habboo@gmail.com>
2022-04-11 14:23:49 -0500

    check saving book

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+// import {setContext} function for apollo client to retrieve the token
+import {setContext} from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+// middleware function to retrieve the token and combine it with the existing httpLink
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token? `Bearer ${token}`: '',
+    },
+  };
+})
+//set the client ti excute the authlink before making the request to our GQL API
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +39,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+
+const{authMiddleware} = require('./utils/auth')
+
+const {typeDefs, resolvers } = require('./schemas')
 const db = require("./config/connection");
-const routes = require("./routes");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const startServer =  async () => {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context:authMiddleware
 });
+await server.start()
 
 server.applyMiddleware({ app });
+
+console.log(`🚀 Your GQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+startServer();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`🚀 Your GQL at http://localhost:${PORT}${server.graphqlPath}`);
+
     console.log(`🌍 Now listening on localhost:${PORT}`);
   });
 });
